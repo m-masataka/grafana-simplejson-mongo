a09c0b31e90dc34d38583d926b753d3c5ca8ef3c
Masataka Mizukoshi <m.mizukoshi.wakuwaku@gmail.com>
2018-02-02 17:14:14 +0000

    update time query

--- api/handler.go
+++ api/handler.go
@@ -2,16 +2,32 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/m-masataka/grafana-simplejson-mongo/mongodb"
 )
+
+type TSQuery struct {
+	DB         string
+	Collection string
+	Col        string
+	TimeCol    string
+	From       time.Time
+	To         time.Time
+	Interval   string
+}
 
 func checkRequest(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
 func reqSearch(w http.ResponseWriter, r *http.Request) {
+	log.Println("Search Query")
 	/*fake*/
 	var result SearchRequest
 	if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
@@ -29,35 +45,174 @@ func reqSearch(w http.ResponseWriter, r *http.Request) {
 }
 
 func (conf *Config) reqQuery(w http.ResponseWriter, r *http.Request) {
+	log.Println("Time Series Query")
 	var result TimeSeriesQuery
 	if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 	sp := mongodb.NewSession(conf.MongoHost)
+	var q TSQuery
+	err := q.parseRangeRaw(result.RangeRaw.From, result.RangeRaw.To)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	q.Interval = result.Interval
 	var resp []TimeSeriesResponse
 	for i, v := range result.Targets {
-		var err error
 		resp = append(resp, TimeSeriesResponse{Target: v.Target})
-		resp[i].DataPoint, err = sp.GetTimeSeriesData("fluentd", v.Target, v.Target, "time")
+		err := q.parseTarget(v.Target)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		resp[i].DataPoint, err = sp.GetTimeSeriesData(q.DB, q.Collection, q.Col, q.TimeCol, q.From, q.To, q.Interval)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	}
 	bytes, err := json.Marshal(resp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	w.Write(bytes)
 }
 
-func addTarget(tqs []TimeSeriesResponse, name string) {
-	target := TimeSeriesResponse{Target: name}
-	tqs = append(tqs, target)
+func (q *TSQuery) parseTarget(target string) error {
+	res := strings.Split(target, ".")
+	if len(res) < 4 {
+		return ERRFormat
+	}
+	q.DB = res[0]
+	q.Collection = res[1]
+	q.Col = res[2]
+	q.TimeCol = res[3]
+	return nil
 }
 
-func (tq *TimeSeriesResponse) addDataPoint(x float64, y float64) {
-	array := make([]float64, 2)
-	array[0] = x
-	array[1] = y
-	tq.DataPoint = append(tq.DataPoint, array)
+var (
+	ERRTimePerDay = errors.New("TimePerDay")
+	ERRFormat     = errors.New("Time format does not match")
+	ERRSameTo     = errors.New("To is same as From")
+)
+
+func (q *TSQuery) parseRangeRaw(from string, to string) error {
+	t, t2, err := parseTime(from)
+	if err == ERRTimePerDay {
+		now := time.Now()
+		if strings.Contains(from, "d") {
+			q.From = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+			q.To = q.From.AddDate(0, 0, 1)
+		} else if strings.Contains(from, "M") {
+			q.From = time.Date(now.Year(), now.Month(), 1, 1, 0, 0, 0, time.UTC)
+			q.To = q.From.AddDate(0, 1, 0)
+		} else if strings.Contains(from, "y") {
+			q.From = time.Date(now.Year(), 1, 1, 1, 0, 0, 0, time.UTC)
+			q.To = q.From.AddDate(1, 0, 0)
+		} else if strings.Contains(from, "w") {
+			_, thisWeek := now.ISOWeek()
+			thisDay := time.Date(now.Year(), now.Month(), now.Day(), 1, 0, 0, 0, time.UTC)
+			q.From = thisDay.AddDate(0, 0, -thisWeek)
+			q.To = thisDay.AddDate(0, 0, 7-thisWeek)
+		} else {
+			return ERRFormat
+		}
+		return nil
+	} else if err == ERRSameTo {
+		q.From = t
+		q.To = t2
+		return nil
+	} else if err != nil {
+		return err
+	}
+	q.From = t
+
+	t, _, err = parseTime(to)
+	if err != nil {
+		return err
+	}
+	q.To = t
+	return nil
+}
+
+func parseTime(str string) (time.Time, time.Time, error) {
+	if str == "now" {
+		return time.Now(), time.Time{}, nil
+	}
+
+	if strings.Contains(str, "Z") {
+		layout := "2006-01-02T15:04:05.000Z"
+		t, err := time.Parse(layout, str)
+		return t, time.Time{}, err
+	}
+
+	if strings.Contains(str, "-") {
+		v := strings.Split(str, "-")
+		now := time.Now()
+		var d time.Duration
+		var err error
+		if strings.Contains(v[1], "d") {
+			var trim string
+			var subtime time.Duration
+			if strings.Contains(v[1], "/") {
+				trim = strings.TrimRight(v[1], "/d")
+				beginningOfTheDay := time.Date(now.Year(), now.Month(), now.Day(), 1, 1, 1, 1, time.UTC)
+				subtime = time.Now().Sub(beginningOfTheDay)
+			} else {
+				trim = v[1]
+			}
+			num, err := strconv.Atoi(strings.TrimRight(trim, "d"))
+			if err != nil {
+				return time.Time{}, time.Time{}, err
+			}
+			d = (time.Duration(num) * time.Hour * 24) - subtime
+			return now.Add(-d), now.Add(-subtime), ERRSameTo
+		} else if strings.Contains(v[1], "M") {
+			var trim string
+			var subtime time.Duration
+			if strings.Contains(v[1], "/") {
+				trim = strings.TrimRight(v[1], "/M")
+				beginningOfTheMonth := time.Date(now.Year(), now.Month(), 1, 1, 1, 1, 1, time.UTC)
+				subtime = time.Now().Sub(beginningOfTheMonth)
+			} else {
+				trim = v[1]
+			}
+			num, err := strconv.Atoi(strings.TrimRight(trim, "M"))
+			if err != nil {
+				return time.Time{}, time.Time{}, err
+			}
+			d = (time.Duration(num) * time.Hour * 24 * 30) - subtime
+			return now.Add(-d), now.Add(-subtime), ERRSameTo
+		} else if strings.Contains(v[1], "y") {
+			var trim string
+			var subtime time.Duration
+			if strings.Contains(v[1], "/") {
+				trim = strings.TrimRight(v[1], "/y")
+				beginningOfTheYear := time.Date(now.Year(), 1, 1, 1, 1, 1, 1, time.UTC)
+				subtime = time.Now().Sub(beginningOfTheYear)
+			} else {
+				trim = v[1]
+			}
+			num, err := strconv.Atoi(strings.TrimRight(trim, "y"))
+			if err != nil {
+				return time.Time{}, time.Time{}, err
+			}
+			d = (time.Duration(num) * time.Hour * 24 * 365) - subtime
+			return now.Add(-d), now.Add(-subtime), ERRSameTo
+		} else {
+			d, err = time.ParseDuration(v[1])
+			if err != nil {
+				return time.Time{}, time.Time{}, err
+			}
+			return now.Add(-d), time.Time{}, nil
+		}
+	}
+
+	if strings.Contains(str, "/") {
+		return time.Time{}, time.Time{}, ERRTimePerDay
+	}
+
+	return time.Time{}, time.Time{}, ERRFormat
 }
--- mongodb/mongodb.go
+++ mongodb/mongodb.go
@@ -1,7 +1,10 @@
 package mongodb
 
 import (
+	"errors"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	"gopkg.in/mgo.v2"
@@ -23,19 +26,34 @@ func NewSession(host string) SessionProvider {
 	return sp
 }
 
-func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, col string, time string) ([][]float64, error) {
+func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, col string, time string, from time.Time, to time.Time, interval string) ([][]float64, error) {
 	var res [][]float64
 	c := sp.Session.DB(dbname).C(collection)
 	var results []bson.M
-	sel := bson.M{"_id": 0, col: 1, time: 1}
-	err := c.Find(nil).Select(sel).Sort("-timestamp").All(&results)
+	pipeline, err := BuildTimeSeriesPipe(col, time, from, to, interval)
+	err = c.Pipe(pipeline).All(&results)
 	if err != nil {
 		return res, err
 	}
 	for _, v := range results {
 		array := make([]float64, 2)
+		num := 0
+		if strings.Contains(interval, "h") {
+			num = 3
+		} else if strings.Contains(interval, "m") {
+			num = 4
+		} else if strings.Contains(interval, "s") {
+			num = 5
+		}
+		date, err := parseDate(v, num)
+		if err == ERRNilDataPoint {
+			log.Println("Contain invalid data")
+			continue
+		} else if err != nil {
+			return res, err
+		}
 		array[0] = convertFloat(v[col])
-		array[1] = convertFloat(v[time])
+		array[1] = convertFloat(date)
 		res = append(res, array)
 	}
 	return res, nil
@@ -52,4 +70,186 @@ func convertFloat(v interface{}) float64 {
 		r = float64(v.(time.Time).UnixNano() / int64(time.Millisecond))
 	}
 	return r
+}
+
+var (
+	ERRNilDataPoint = errors.New("NilDataPoint")
+)
+
+func parseDate(v bson.M, num int) (time.Time, error) {
+	var year, month, day, hour, minute, second, milisec int
+	for i := 0; i <= num; i++ {
+		switch i {
+		case 0:
+			if v["_id"].(bson.M)["year"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			year = v["_id"].(bson.M)["year"].(int)
+		case 1:
+			if v["_id"].(bson.M)["month"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			month = v["_id"].(bson.M)["month"].(int)
+		case 2:
+			if v["_id"].(bson.M)["day"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			day = v["_id"].(bson.M)["day"].(int)
+		case 3:
+			if v["_id"].(bson.M)["hour"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			hour = v["_id"].(bson.M)["hour"].(int)
+		case 4:
+			if v["_id"].(bson.M)["minute"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			minute = v["_id"].(bson.M)["minute"].(int)
+		case 5:
+			if v["_id"].(bson.M)["second"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			second = v["_id"].(bson.M)["second"].(int)
+		case 6:
+			if v["_id"].(bson.M)["milisecond"] == nil {
+				return time.Time{}, ERRNilDataPoint
+			}
+			milisec = v["_id"].(bson.M)["milisecond"].(int)
+		}
+	}
+	return time.Date(year, time.Month(month), day, hour, minute, second, milisec, time.UTC), nil
+}
+
+func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Time, interval string) ([]bson.M, error) {
+	var uni string
+	var num int
+	var err error
+	if strings.Contains(interval, "s") {
+		uni = "$second"
+		num, err = strconv.Atoi(strings.TrimRight(interval, "s"))
+		if err != nil {
+			return []bson.M{}, err
+		}
+	} else if strings.Contains(interval, "m") {
+		uni = "$minute"
+		num, err = strconv.Atoi(strings.TrimRight(interval, "m"))
+		if err != nil {
+			return []bson.M{}, err
+		}
+	} else if strings.Contains(interval, "h") {
+		uni = "$hour"
+		num, err = strconv.Atoi(strings.TrimRight(interval, "h"))
+		if err != nil {
+			return []bson.M{}, err
+		}
+	}
+	trange := bson.M{timecol: bson.M{"$gte": from, "$lte": to}}
+	timeBson := buildTimeBson(uni, timecol, num)
+	pipeline := []bson.M{
+		{
+			"$match": trange,
+		},
+		{
+			"$group": bson.M{
+				"_id": timeBson,
+				col: bson.M{
+					"$avg": "$" + col,
+				},
+			},
+		},
+		{
+			"$sort": bson.M{
+				"_id": 1,
+			},
+		},
+	}
+	return pipeline, nil
+}
+
+func buildTimeBson(uni string, timecol string, interval int) bson.M {
+	mod := []interface{}{bson.M{uni: "$" + timecol}, interval}
+	submod := []interface{}{bson.M{uni: "$" + timecol}, bson.M{"$mod": mod}}
+	var ret bson.M
+	switch uni {
+	case "$hour":
+		ret = bson.M{
+			"year": bson.M{
+				"$year": "$" + timecol,
+			},
+			"month": bson.M{
+				"$month": "$" + timecol,
+			},
+			"day": bson.M{
+				"$dayOfMonth": "$" + timecol,
+			},
+			"hour": bson.M{
+				"$subtract": submod,
+			},
+		}
+	case "$minute":
+		ret = bson.M{
+			"year": bson.M{
+				"$year": "$" + timecol,
+			},
+			"month": bson.M{
+				"$month": "$" + timecol,
+			},
+			"day": bson.M{
+				"$dayOfMonth": "$" + timecol,
+			},
+			"hour": bson.M{
+				"$hour": "$" + timecol,
+			},
+			"minute": bson.M{
+				"$subtract": submod,
+			},
+		}
+	case "$second":
+		ret = bson.M{
+			"year": bson.M{
+				"$year": "$" + timecol,
+			},
+			"month": bson.M{
+				"$month": "$" + timecol,
+			},
+			"day": bson.M{
+				"$dayOfMonth": "$" + timecol,
+			},
+			"hour": bson.M{
+				"$hour": "$" + timecol,
+			},
+			"minute": bson.M{
+				"$minute": "$" + timecol,
+			},
+			"second": bson.M{
+				"$subtract": submod,
+			},
+		}
+	case "$milisecond":
+		ret = bson.M{
+			"year": bson.M{
+				"$year": "$" + timecol,
+			},
+			"month": bson.M{
+				"$month": "$" + timecol,
+			},
+			"day": bson.M{
+				"$dayOfMonth": "$" + timecol,
+			},
+			"hour": bson.M{
+				"$hour": "$" + timecol,
+			},
+			"minute": bson.M{
+				"$minute": "$" + timecol,
+			},
+			"second": bson.M{
+				"$second": "$" + timecol,
+			},
+			"millisecond": bson.M{
+				"$subtract": submod,
+			},
+		}
+
+	}
+	return ret
 }
