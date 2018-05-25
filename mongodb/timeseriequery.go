8e67ad74198dadad45eece1fdf9e1d73ac1f629b
Masataka Mizukoshi <m.mizukoshi.wakuwaku@gmail.com>
2018-05-25 16:50:43 +0000

    support string to date

--- mongodb/timeseriequery.go
+++ mongodb/timeseriequery.go
@@ -3,16 +3,28 @@ package mongodb
 import (
 	"log"
 	"time"
+	"reflect"
 
 	"gopkg.in/mgo.v2/bson"
 )
 
 func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, col string, timecol string, from time.Time, to time.Time, intervalMs int) ([][]float64, error) {
 	var res [][]float64
+	var strflag bool
 	c := sp.Session.DB(dbname).C(collection)
 	var results []bson.M
-	pipeline := BuildTimeSeriesPipe(col, timecol, from, to, intervalMs)
-	err := c.Pipe(pipeline).All(&results)
+	var judge bson.M
+	err := c.Find(nil).One(&judge)
+	if err != nil {
+		return res, err
+	}
+	if reflect.TypeOf(judge[timecol]).Kind()  == reflect.String {
+		strflag = true
+	}else {
+		strflag = false
+	}
+	pipeline := BuildTimeSeriesPipe(col, timecol, from, to, intervalMs, strflag)
+	err = c.Pipe(pipeline).All(&results)
 	if err != nil {
 		return res, err
 	}
@@ -33,13 +45,18 @@ func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, c
 }
 
 
-func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Time, intervalMs int) ([]bson.M) {
-	trange := bson.M{timecol: bson.M{"$gte": from, "$lte": to}}
+func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Time, intervalMs int, strflag bool) ([]bson.M) {
+	var trange bson.M
+	if strflag {
+		trange = bson.M{ timecol: bson.M{"$gte": from.Format("20060102150405"), "$lte": to.Format("20060102150405")}}
+	} else {
+		trange = bson.M{ timecol: bson.M{"$gte": from, "$lte": to}}
+	}
 	pipeline := []bson.M{
 		{ "$match": trange },
 		{
 			"$group": bson.M{
-				"_id": buildTimeBson(timecol, intervalMs) ,
+				"_id": buildTimeBson(timecol, intervalMs, strflag) ,
 				"value": bson.M{"$avg": "$" + col},
 			},
 		},
@@ -52,82 +69,94 @@ func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Tim
 	return pipeline
 }
 //{ "$sort": bson.M{ "_id": 1}},
-func buildTimeBson(timecol string, intervalMs int) bson.M {
+func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
 	var ret bson.M
+	var TimeCol interface{}
+	if strflag {
+		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
+	} else {
+		TimeCol = "$" + timecol
+	}
 	if 86400000 <= intervalMs && intervalMs < 2629800000 {
 		uni := "$day"
 		return bson.M{
 			"year": bson.M{
-				"$year": "$" + timecol,
+				"$year": TimeCol,
 			},
 			"month": bson.M{
-				"$month": "$" + timecol,
+				"$month": TimeCol,
 			},
-			"day": buildInterval(timecol, intervalMs, uni, 86400000),
+			"day": buildInterval(timecol, intervalMs, uni, 86400000, strflag),
 		}
 	} else if 3600000 <= intervalMs && intervalMs< 86400000 {
 		uni := "$hour"
 		return bson.M{
 			"year": bson.M{
-				"$year": "$" + timecol,
+				"$year": TimeCol,
 			},
 			"month": bson.M{
-				"$month": "$" + timecol,
+				"$month": TimeCol,
 			},
 			"day": bson.M{
-				"$dayOfMonth": "$" + timecol,
+				"$dayOfMonth": TimeCol,
 			},
-			"hour": buildInterval(timecol, intervalMs, uni, 3600000),
+			"hour": buildInterval(timecol, intervalMs, uni, 3600000, strflag),
 		}
 	} else if 60000 <= intervalMs && intervalMs< 3600000 {
 		uni := "$minute"
 		return bson.M{
 			"year": bson.M{
-				"$year": "$" + timecol,
+				"$year": TimeCol,
 			},
 			"month": bson.M{
-				"$month": "$" + timecol,
+				"$month": TimeCol,
 			},
 			"day": bson.M{
-				"$dayOfMonth": "$" + timecol,
+				"$dayOfMonth": TimeCol,
 			},
 			"hour": bson.M{
-				"$hour": "$" + timecol,
+				"$hour": TimeCol,
 			},
-			"minute": buildInterval(timecol, intervalMs, uni, 60000),
+			"minute": buildInterval(timecol, intervalMs, uni, 60000, strflag),
 		}
 	} else {
 		uni := "$second"
 		return bson.M{
 			"year": bson.M{
-				"$year": "$" + timecol,
+				"$year": TimeCol,
 			},
 			"month": bson.M{
-				"$month": "$" + timecol,
+				"$month": TimeCol,
 			},
 			"day": bson.M{
-				"$dayOfMonth": "$" + timecol,
+				"$dayOfMonth": TimeCol,
 			},
 			"hour": bson.M{
-				"$hour": "$" + timecol,
+				"$hour": TimeCol,
 			},
 			"minute": bson.M{
-				"$minute": "$" + timecol,
+				"$minute": TimeCol,
 			},
-			"interval": buildInterval(timecol, intervalMs, uni, 1000),
+			"interval": buildInterval(timecol, intervalMs, uni, 1000, strflag),
 		}
 	}
 	return ret
 }
 
-func buildInterval( timecol string, intervalMs int, uni string, ms int) bson.M {
+func buildInterval( timecol string, intervalMs int, uni string, ms int, strflag bool) bson.M {
 	type list []interface{}
 	interval := intervalMs/ms
 	if interval < 1{
 		interval = 1
 	}
-	mod := list{ bson.M{uni: "$" + timecol}, interval}
-	sub := list{ bson.M{uni: "$" + timecol}, bson.M{"$mod": mod } }
+	var TimeCol bson.M
+	if strflag {
+		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
+	} else {
+		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
+	}
+	mod := list{ bson.M{uni: TimeCol}, interval}
+	sub := list{ bson.M{uni: TimeCol}, bson.M{"$mod": mod } }
 	return bson.M{"$subtract": sub}
 }
 
