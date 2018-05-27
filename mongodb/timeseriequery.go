8055c75de55e1fb36e89bdc45006d0e87625420d
Masataka Mizukoshi <m.mizukoshi.wakuwaku@gmail.com>
2018-05-27 04:55:37 +0000

    add int to date

--- mongodb/timeseriequery.go
+++ mongodb/timeseriequery.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"time"
 	"reflect"
+	"strconv"
 
 	"gopkg.in/mgo.v2/bson"
 )
 
 func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, col string, timecol string, from time.Time, to time.Time, intervalMs int) ([][]float64, error) {
 	var res [][]float64
-	var strflag bool
 	c := sp.Session.DB(dbname).C(collection)
 	var results []bson.M
 	var judge bson.M
@@ -18,24 +18,29 @@ func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, c
 	if err != nil {
 		return res, err
 	}
-	if reflect.TypeOf(judge[timecol]).Kind()  == reflect.String {
-		strflag = true
-	}else {
-		strflag = false
-	}
-	pipeline := BuildTimeSeriesPipe(col, timecol, from, to, intervalMs, strflag)
+	timecolType := reflect.TypeOf(judge[timecol]).Kind()
+	pipeline := BuildTimeSeriesPipe(col, timecol, from, to, intervalMs, timecolType)
 	err = c.Pipe(pipeline).All(&results)
 	if err != nil {
 		return res, err
 	}
 	for _, v := range results {
 		array := make([]float64, 2)
-		date, err := parseIdtoDate(v, intervalMs)
-		if err == ERRNilDataPoint {
-			log.Println("Contain invalid data")
-			continue
-		} else if err != nil {
-			return res, err
+		var date time.Time
+		switch timecolType {
+		case reflect.Int ,reflect.Float64:
+			date, err =  parseInttoDate(int(v["_id"].(float64)))
+			if err != nil {
+				return res, err
+			}
+		default:
+			date, err = parseIdtoDate(v, intervalMs)
+			if err == ERRNilDataPoint {
+				log.Println("Contain invalid data")
+				continue
+			} else if err != nil {
+				return res, err
+			}
 		}
 		array[0] = convertFloat(v["value"])
 		array[1] = convertFloat(date)
@@ -45,18 +50,23 @@ func (sp *SessionProvider) GetTimeSeriesData(dbname string, collection string, c
 }
 
 
-func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Time, intervalMs int, strflag bool) ([]bson.M) {
+func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Time, intervalMs int, timecolType reflect.Kind) ([]bson.M) {
 	var trange bson.M
-	if strflag {
+	switch timecolType {
+	case reflect.String:
 		trange = bson.M{ timecol: bson.M{"$gte": from.Format("20060102150405"), "$lte": to.Format("20060102150405")}}
-	} else {
+	case reflect.Int ,reflect.Float64:
+		intFrom, _ := strconv.Atoi(from.Format("20060102150405"))
+		intTo, _ := strconv.Atoi(to.Format("20060102150405"))
+		trange = bson.M{ timecol: bson.M{"$gte": intFrom, "$lte": intTo}}
+	default:
 		trange = bson.M{ timecol: bson.M{"$gte": from, "$lte": to}}
 	}
 	pipeline := []bson.M{
 		{ "$match": trange },
 		{
 			"$group": bson.M{
-				"_id": buildTimeBson(timecol, intervalMs, strflag) ,
+				"_id": buildTimeBson(timecol, intervalMs, timecolType) ,
 				"value": bson.M{"$avg": "$" + col},
 			},
 		},
@@ -69,12 +79,19 @@ func BuildTimeSeriesPipe(col string, timecol string, from time.Time, to time.Tim
 	return pipeline
 }
 //{ "$sort": bson.M{ "_id": 1}},
-func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
+func buildTimeBson(timecol string, intervalMs int, timecolType reflect.Kind) bson.M {
 	var ret bson.M
 	var TimeCol interface{}
-	if strflag {
+	switch timecolType {
+	case reflect.String:
 		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
-	} else {
+	case reflect.Int, reflect.Float64:
+		interval := intervalMs/1000
+		if interval < 1 {
+			interval = 1
+		}
+		return bson.M{ "$subtract" : []interface{}{"$" + timecol, bson.M{ "$mod": []interface{}{"$"+timecol , interval}}}}
+	default:
 		TimeCol = "$" + timecol
 	}
 	if 86400000 <= intervalMs && intervalMs < 2629800000 {
@@ -86,7 +103,7 @@ func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
 			"month": bson.M{
 				"$month": TimeCol,
 			},
-			"day": buildInterval(timecol, intervalMs, uni, 86400000, strflag),
+			"day": buildInterval(timecol, intervalMs, uni, 86400000, timecolType),
 		}
 	} else if 3600000 <= intervalMs && intervalMs< 86400000 {
 		uni := "$hour"
@@ -100,7 +117,7 @@ func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
 			"day": bson.M{
 				"$dayOfMonth": TimeCol,
 			},
-			"hour": buildInterval(timecol, intervalMs, uni, 3600000, strflag),
+			"hour": buildInterval(timecol, intervalMs, uni, 3600000, timecolType),
 		}
 	} else if 60000 <= intervalMs && intervalMs< 3600000 {
 		uni := "$minute"
@@ -117,7 +134,7 @@ func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
 			"hour": bson.M{
 				"$hour": TimeCol,
 			},
-			"minute": buildInterval(timecol, intervalMs, uni, 60000, strflag),
+			"minute": buildInterval(timecol, intervalMs, uni, 60000, timecolType),
 		}
 	} else {
 		uni := "$second"
@@ -137,26 +154,27 @@ func buildTimeBson(timecol string, intervalMs int, strflag bool) bson.M {
 			"minute": bson.M{
 				"$minute": TimeCol,
 			},
-			"interval": buildInterval(timecol, intervalMs, uni, 1000, strflag),
+			"interval": buildInterval(timecol, intervalMs, uni, 1000, timecolType),
 		}
 	}
 	return ret
 }
 
-func buildInterval( timecol string, intervalMs int, uni string, ms int, strflag bool) bson.M {
+func buildInterval( timecol string, intervalMs int, uni string, ms int, timecolType reflect.Kind) bson.M {
 	type list []interface{}
 	interval := intervalMs/ms
 	if interval < 1{
 		interval = 1
 	}
-	var TimeCol bson.M
-	if strflag {
-		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
-	} else {
-		TimeCol = bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}
+	var uniTime bson.M
+	switch timecolType{
+	case reflect.String:
+		uniTime = bson.M{ uni: bson.M{ "$dateFromString": bson.M{ "dateString": "$" + timecol}}}
+	default:
+		uniTime = bson.M{ uni: "$" + timecol}
 	}
-	mod := list{ bson.M{uni: TimeCol}, interval}
-	sub := list{ bson.M{uni: TimeCol}, bson.M{"$mod": mod } }
+	mod := list{ uniTime, interval}
+	sub := list{ uniTime, bson.M{"$mod": mod } }
 	return bson.M{"$subtract": sub}
 }
 
@@ -213,3 +231,6 @@ func parseIdtoDate(v bson.M, intervalMs int) (time.Time, error){
 	return time.Date(year, time.Month(month), day, hour, minute, second, 0, time.UTC), nil
 }
 
+func parseInttoDate(date int) (time.Time, error) {
+	return time.Date(date/10000000000, time.Month((date/100000000)%100) , (date/1000000)%100, (date/10000)%100, (date/100)%100, date%100, 0, time.UTC), nil
+}
